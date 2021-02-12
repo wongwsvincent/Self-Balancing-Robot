69431b173c75d26d71c8b92fa9da4224e8c3d9ac
vwong <wongwsvincent@gmail.com>
2021-02-12 10:30:23 -0800

    Update SARSA and Q-learning

--- Emulator/models/agentQlearning.py
+++ Emulator/models/agentQlearning.py
@@ -1,14 +1,18 @@
 import numpy as np
 import random
+import math
 
 class AgentQlearning(object):
-    def __init__(self, env, alpha=0.1, gamma=0.9, epsilon=0.8):
+    def __init__(self, env, alpha=0.1, gamma=0.9, epsilon=1.0, epsilon_lifetime=10):
         self.env = env
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
+        self.epsilon_lifetime = epsilon_lifetime
         self.q_table = {}
         self.actions = env.action_space
+        self.visits = {}
+        self.training = True
     
     def get_q_table(self):
         return self.q_table
@@ -18,21 +22,28 @@ class AgentQlearning(object):
         
     def set_epsilon(self, epsilon):
         self.epsilon=epsilon
+
+    def set_epsilon_lifetime(self, epsilon_lifetime):
+        self.epsilon_lifetime=epsilon_lifetime
     
+    def train_mode(self, flag):
+        self.training=flag
+
     def check_if_state_exist(self, state):
         if state not in self.q_table:
             self.q_table[state]=np.zeros(self.actions.n)
+            self.visits[state]=0
             
     def get_action(self, state):
         self.check_if_state_exist(state)
-        if np.random.rand() > self.epsilon:
+        if self.training==True and np.random.rand() < math.exp(-self.visits[state]/self.epsilon_lifetime)*self.epsilon:
+            target_action = self.actions.sample()
+        else:
             target_actions = self.q_table[state]
             idx_list=list(range(len(target_actions)))
             random.shuffle(idx_list)
             reordered=target_actions[idx_list]
             target_action = idx_list[np.argmax(reordered)]
-        else:
-            target_action = self.actions.sample()
         return target_action
 
     def update_q_table(self, state, action, reward, state_next, terminal):
@@ -43,6 +54,7 @@ class AgentQlearning(object):
         else:
             q_value_real = reward
         self.q_table[state][action] += self.alpha * (q_value_real - q_value_predict)
+        self.visits[state] += 1
 
     def train(self, state):
         # Get first action.
@@ -52,5 +64,5 @@ class AgentQlearning(object):
         # Get next action.
         action_next = self.get_action(state_next)
         # Update Q table.
-        self.update_q_table(state, action, reward, state_next, action_next, terminal)
+        self.update_q_table(state, action, reward, state_next, terminal)
         return state_next, reward, terminal 
--- Emulator/models/agentSarsa.py
+++ Emulator/models/agentSarsa.py
@@ -1,14 +1,18 @@
 import numpy as np
 import random
+import math
 
 class AgentSarsa(object):
-    def __init__(self, env, alpha=0.1, gamma=0.9, epsilon=0.9):
+    def __init__(self, env, alpha=0.1, gamma=0.9, epsilon=1.0, epsilon_lifetime=10):
         self.env = env
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
+        self.epsilon_lifetime = epsilon_lifetime
         self.q_table = {}
         self.actions = env.action_space
+        self.visits = {}
+        self.training = True
     
     def get_q_table(self):
         return self.q_table
@@ -19,20 +23,27 @@ class AgentSarsa(object):
     def set_epsilon(self, epsilon):
         self.epsilon=epsilon
     
+    def set_epsilon_lifetime(self, epsilon_lifetime):
+        self.epsilon_lifetime=epsilon_lifetime
+
+    def train_mode(self, flag):
+        self.training=flag
+
     def check_if_state_exist(self, state):
         if state not in self.q_table:
             self.q_table[state]=np.zeros(self.actions.n)
+            self.visits[state]=0
             
     def get_action(self, state):
         self.check_if_state_exist(state)
-        if np.random.rand() > self.epsilon:
+        if self.training==True and np.random.rand() < math.exp(-self.visits[state]/self.epsilon_lifetime)*self.epsilon:
+            target_action = self.actions.sample()
+        else:
             target_actions = self.q_table[state]
             idx_list=list(range(len(target_actions)))
             random.shuffle(idx_list)
             reordered=target_actions[idx_list]
             target_action = idx_list[np.argmax(reordered)]
-        else:
-            target_action = self.actions.sample()
         return target_action
 
     def update_q_table(self, state, action, reward, state_next, action_next, terminal):
@@ -43,6 +54,7 @@ class AgentSarsa(object):
         else:
             q_value_real = reward
         self.q_table[state][action] += self.alpha * (q_value_real - q_value_predict)
+        self.visits[state] += 1
 
     def train(self, state):
         # Get first action.
