2ddb3793d58503573936c034d3f32d46685a3a18
vwong <wongwsvincent@gmail.com>
2021-02-14 14:13:41 -0800

    update Qlearning_ER

--- Emulator/models/agentQlearning_ER.py
+++ Emulator/models/agentQlearning_ER.py
@@ -4,7 +4,7 @@ import math
 from collections import deque
 
 class AgentQlearning_ER(object):
-    def __init__(self, env, alpha=0.1, gamma=0.9, epsilon_max=1.0, epsilon_min=0.2, epsilon_halflife=10, memory_size=100000, memory_sampling=10):
+    def __init__(self, env, alpha=0.1, gamma=0.9, epsilon_max=1.0, epsilon_min=0.2, epsilon_halflife=10, memory_size=100000, memory_sampling=10, update_freq=100):
         self.env = env
         self.actions = env.action_space
         self.alpha = alpha
@@ -18,6 +18,8 @@ class AgentQlearning_ER(object):
         self.memory_size = memory_size
         self.memory_sampling = memory_sampling
         self.memory_table = deque(maxlen=self.memory_size)
+        self.update_freq = 100
+        self.counter = 0
     
     def get_q_table(self):
         return self.q_table
@@ -71,11 +73,14 @@ class AgentQlearning_ER(object):
         state_next, reward, terminal, info = self.env.step(action)
         # Store transition experience in memory
         self.memory_table.append((state, action, reward, state_next, terminal))
-        # Sample random minibatch of transition experiences from memory
-        experiences = np.random.choice(len(self.memory_table), size=min(len(self.memory_table),self.memory_sampling), replace=False)
-        # Update Q table with past experiences
-        for exp_idx in experiences:
-            self.update_q_table(self.memory_table[exp_idx][0], self.memory_table[exp_idx][1], self.memory_table[exp_idx][2], self.memory_table[exp_idx][3], self.memory_table[exp_idx][4])
-        # Update Q table with the latest experience
-        self.update_q_table(state, action, reward, state_next, terminal)
+        self.counter+=1
+        if (self.counter%self.update_freq==0):
+            # Sample random minibatch of transition experiences from memory
+            experiences = np.random.choice(len(self.memory_table), size=min(len(self.memory_table),self.memory_sampling), replace=False)
+            # Update Q table with past experiences
+            for exp_idx in experiences:
+                self.update_q_table(self.memory_table[exp_idx][0], self.memory_table[exp_idx][1], self.memory_table[exp_idx][2], self.memory_table[exp_idx][3], self.memory_table[exp_idx][4])
+            # Update Q table with the latest experience
+            self.update_q_table(state, action, reward, state_next, terminal)
+            self.counter=0
         return state_next, reward, terminal 
